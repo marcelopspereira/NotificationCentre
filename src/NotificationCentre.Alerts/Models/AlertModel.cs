bf533ff43bfc32acda28086f2af654f3d7dd0aea
Mark <mark.mcdowell@me.com>
2016-11-03 07:58:41 +0000

    raising property changed

--- src/NotificationCentre.Alerts/Models/AlertModel.cs
+++ src/NotificationCentre.Alerts/Models/AlertModel.cs
@@ -1,22 +1,96 @@
 ﻿using System.ComponentModel;
 using System.Windows.Input;
+using Presentation.Core;
 
 namespace NotificationCentre.Alerts.Models
 {
     internal sealed class AlertModel : INotifyPropertyChanged, IAlertModel
     {
+        private string _icon;
+        private string _title;
+        private string _content;
+        private ICommand _timeout;
+        private ICommand _dismiss;
+        private ICommand _action;
+
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public string Icon { get; set; }
+        public string Icon
+        {
+            get { return _icon; }
+            set
+            {
+                if (_icon == value)
+                    return;
 
-        public string Title { get; set; }
+                _icon = value;
+                PropertyChanged.Raise(this);
+            }
+        }
 
-        public string Content { get; set; }
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (_title == value)
+                    return;
 
-        public ICommand Timeout { get; set; }
+                _title = value;
+                PropertyChanged.Raise(this);
+            }
+        }
 
-        public ICommand Dismiss { get; set; }
+        public string Content
+        {
+            get { return _content; }
+            set
+            {
+                if (_content == value)
+                    return;
 
-        public ICommand Action { get; set; }
+                _content = value;
+                PropertyChanged.Raise(this);
+            }
+        }
+
+        public ICommand Timeout
+        {
+            get { return _timeout; }
+            set
+            {
+                if (_timeout == value)
+                    return;
+
+                _timeout = value;
+                PropertyChanged.Raise(this);
+            }
+        }
+
+        public ICommand Dismiss
+        {
+            get { return _dismiss; }
+            set
+            {
+                if (_dismiss == value)
+                    return;
+                
+                _dismiss = value;
+                PropertyChanged.Raise(this);
+            }
+        }
+
+        public ICommand Action
+        {
+            get { return _action; }
+            set
+            {
+                if (_action == value)
+                    return;
+
+                _action = value;
+                PropertyChanged.Raise(this);
+            }
+        }
     }
 }
