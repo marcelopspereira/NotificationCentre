5e8b89bbd69f7b9e112314db674b08b92c26ef30
Mark <mark.mcdowell@me.com>
2016-11-25 20:55:16 +0000

    adding logging to main project

--- src/NotificationCentre/App.xaml.cs
+++ src/NotificationCentre/App.xaml.cs
@@ -3,11 +3,13 @@ using System.ComponentModel.Composition.Hosting;
 using System.Windows;
 using Presentation.Bootstrappers;
 using Presentation.Interfaces;
+using NLog;
 
 namespace NotificationCentre
 {    
     public partial class App
     {
+        private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
         private readonly IBootstrapper _bootstrapper;
 
         public App()
@@ -19,9 +21,20 @@ namespace NotificationCentre
             var bootstrapper = new DelegateBootstrapper(() => {}, container.Dispose);
 
             _bootstrapper = bootstrapper.InitalizeModules(container)
-                                        .CatchDispatcherExceptions(this, ex => true)
-                                        .CatchTaskExceptions(ex => true)
-                                        .CatchAppDomainExceptions(AppDomain.CurrentDomain, (ex, handled) => {});
+                                        .CatchDispatcherExceptions(this, ex =>
+                                        {
+                                            _logger.Error(ex);
+                                            return true;
+                                        })
+                                        .CatchTaskExceptions(ex => 
+                                        {
+                                            _logger.Error(ex);
+                                            return true;
+                                        })
+                                        .CatchAppDomainExceptions(AppDomain.CurrentDomain, (ex, handled) => 
+                                        {
+                                            _logger.Error(ex);
+                                        });
         }
 
         protected override void OnStartup(StartupEventArgs e)
